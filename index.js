36c4b3541448566b362b5413084b243e94e88002
ESTON NDUNGU <estonndungu1@gmail.com>
2024-06-25 18:19:56 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+// Multiply Variables
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2 // The result should be 62
+
+//Generate a random integer greater than 0
+
+let random = Math.floor(Math.random() * 10) // Math.random generates a number betwwen 0 and 0.999 and we multiply it by  10 to give us a number that is greater than 0
+
+
+// Calculates  a remainder we use modulas (%)
+
+let num3 = 52
+let num4 = 6
+let mod = num3 % num4 // The remainder of dividing 52 by 6 should be 4
+
+// Get maximum number
+let max = Math.max(2,5,20,6,14,18,3,19,10) // Maximum number should be 20
